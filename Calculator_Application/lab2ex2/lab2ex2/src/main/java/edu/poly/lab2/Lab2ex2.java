1e379560d7be5bca909db9ffcf5f628487fa9348
Nhật Long <longrcb01@gmail.com>
2025-04-28 12:07:42 +0700

    Add files via upload

---
+++ Calculator_Application/lab2ex2/lab2ex2/src/main/java/edu/poly/lab2/Lab2ex2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.poly.lab2;
+
+/**
+ *
+ * @author Nhật Long
+ */
+public class Lab2ex2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
